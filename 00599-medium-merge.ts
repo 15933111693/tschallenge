df968cae1a65cc59641466a9a86965959c988ac0
15933111693 <15933111693@163.com>
2023-03-06 17:41:22 +0800

    2023.3.6 commit

--- 00599-medium-merge.ts
+++ 00599-medium-merge.ts
@@ -20,14 +20,14 @@ type cases = [
 
 
 // ============= Your Code Here =============
-type MyPick<T, K extends keyof T> = {
-  [P in K]: T[P];
-};
-type MyOmit<T, K extends keyof any> = MyPick<T, Exclude<keyof T, K>>;
-type Merge<F, S> = MyOmit<F, keyof S> & S;
-// type Merge<F, S> = {
-//   [Key in keyof F | keyof S]: Key extends keyof S ? S[Key] : Key extends keyof F ? F[Key] : never
-// }
+// type MyPick<T, K extends keyof T> = {
+//   [P in K]: T[P];
+// };
+// type MyOmit<T, K extends keyof any> = MyPick<T, Exclude<keyof T, K>>;
+// type Merge<F, S> = MyOmit<F, keyof S> & S;
+type Merge<F, S> = {
+  [Key in keyof F | keyof S]: Key extends keyof S ? S[Key] : Key extends keyof F ? F[Key] : never
+}
 
 type SS = Equal<Merge<Foo, Bar>, {
   a: number
---
+++ 00949-medium-anyof.ts
@@ -0,0 +1,27 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type cases = [
+  Expect<
+    Equal<AnyOf<[1, "test", true, [1], { name: "test" }, { 1: "test" }]>, true>
+  >,
+  Expect<Equal<AnyOf<[1, "", false, [], {}]>, true>>,
+  Expect<Equal<AnyOf<[0, "test", false, [], {}]>, true>>,
+  Expect<Equal<AnyOf<[0, "", true, [], {}]>, true>>,
+  Expect<Equal<AnyOf<[0, "", false, [1], {}]>, true>>,
+  Expect<Equal<AnyOf<[0, "", false, [], { name: "test" }]>, true>>,
+  Expect<Equal<AnyOf<[0, "", false, [], { 1: "test" }]>, true>>,
+  Expect<
+    Equal<AnyOf<[0, "", false, [], { name: "test" }, { 1: "test" }]>, true>
+  >,
+  Expect<Equal<AnyOf<[0, "", false, [], {}, undefined, null]>, false>>,
+  Expect<Equal<AnyOf<[]>, false>>
+];
+
+// ============= Your Code Here =============
+type IsFalse<T> = T extends Record<string, never> | [] | '' | 0 | null | undefined | false ? true : false
+type AnyOf<T extends readonly any[]> = T extends [infer F, ...infer R]
+  ? IsFalse<F> extends true
+    ? AnyOf<R>
+    : true
+  : false;
