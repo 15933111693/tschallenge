4c2981acc809b033fb777ae3d1a21717ec8d1f45
15933111693 <15933111693@163.com>
2023-03-16 23:23:39 +0800

    2023.3.16

---
+++ 02759-medium-requiredbykeys.ts
@@ -0,0 +1,37 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+interface User {
+  name?: string;
+  age?: number;
+  address?: string;
+}
+
+interface UserRequiredName {
+  name: string;
+  age?: number;
+  address?: string;
+}
+
+interface UserRequiredNameAndAge {
+  name: string;
+  age: number;
+  address?: string;
+}
+
+type cases = [
+  Expect<Equal<RequiredByKeys<User, "name">, UserRequiredName>>,
+  Expect<Equal<RequiredByKeys<User, "name" | "age">, UserRequiredNameAndAge>>,
+  Expect<Equal<RequiredByKeys<User>, Required<User>>>,
+  // @ts-expect-error
+  Expect<Equal<RequiredByKeys<User, "name" | "unknown">, UserRequiredName>>
+];
+
+type Merge<T> = {
+  [K in keyof T]: T[K];
+};
+
+// ============= Your Code Here =============
+type RequiredByKeys<T, K extends keyof T = keyof T> = Merge<
+  Omit<T, K> & { [Key in K]-?: T[Key] }
+>;
