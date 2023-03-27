7cbc37136db68f004347499d37d7c21ec149f118
15933111693 <15933111693@163.com>
2023-03-27 19:34:56 +0800

    2023.3.27

--- 04471-medium-zip.ts
+++ 04471-medium-zip.ts
@@ -1,14 +1,27 @@
 // ============= Test Cases =============
-import type { Equal, Expect } from './test-utils'
+import type { Equal, Expect } from "./test-utils";
 
 type cases = [
   Expect<Equal<Zip<[], []>, []>>,
   Expect<Equal<Zip<[1, 2], [true, false]>, [[1, true], [2, false]]>>,
-  Expect<Equal<Zip<[1, 2, 3], ['1', '2']>, [[1, '1'], [2, '2']]>>,
+  Expect<Equal<Zip<[1, 2, 3], ["1", "2"]>, [[1, "1"], [2, "2"]]>>,
   Expect<Equal<Zip<[], [1, 2, 3]>, []>>,
-  Expect<Equal<Zip<[[1, 2]], [3]>, [[[1, 2], 3]]>>,
-]
-
+  Expect<Equal<Zip<[[1, 2]], [3]>, [[[1, 2], 3]]>>
+];
 
 // ============= Your Code Here =============
-type Zip<T, U> 
+type Shift<T extends unknown[]> = T extends [infer F, ...infer R]
+  ? [F, R]
+  : never;
+type Zip<T extends any[], U extends any[]> = Shift<T> extends never
+  ? []
+  : Shift<U> extends never
+  ? []
+  : Shift<T> extends [infer F, infer R]
+  ? Shift<U> extends [infer F1, infer R2]
+    ? [
+        [F, F1],
+        ...(R extends unknown[] ? (R2 extends unknown[] ? Zip<R, R2> : []) : [])
+      ]
+    : []
+  : [];
--- 09286-medium-firstuniquecharindex.ts
+++ 09286-medium-firstuniquecharindex.ts
@@ -1,13 +1,28 @@
 // ============= Test Cases =============
-import type { Equal, Expect } from './test-utils'
+import type { Equal, Expect } from "./test-utils";
 
 type cases = [
-  Expect<Equal<FirstUniqueCharIndex<'leetcode'>, 0>>,
-  Expect<Equal<FirstUniqueCharIndex<'loveleetcode'>, 2>>,
-  Expect<Equal<FirstUniqueCharIndex<'aabb'>, -1>>,
-  Expect<Equal<FirstUniqueCharIndex<''>, -1>>,
-]
-
+  Expect<Equal<FirstUniqueCharIndex<"leetcode">, 0>>,
+  Expect<Equal<FirstUniqueCharIndex<"loveleetcode">, 2>>,
+  Expect<Equal<FirstUniqueCharIndex<"aabb">, -1>>,
+  Expect<Equal<FirstUniqueCharIndex<"">, -1>>
+];
 
 // ============= Your Code Here =============
-type FirstUniqueCharIndex<T extends string> = any
+type Check<
+  T extends string,
+  U extends string
+> = T extends `${infer F}${infer R}`
+  ? F extends U
+    ? false
+    : Check<R, U>
+  : true;
+type FirstUniqueCharIndex<
+  T extends string,
+  Ans extends string = '',
+  Ans2 extends unknown[] = []
+> = T extends `${infer F}${infer R}`
+  ? Check<`${Ans}${R}`, F> extends true
+    ? Ans2['length']
+    : FirstUniqueCharIndex<R, `${Ans}${F}`, [...Ans2, 0]>
+  : -1;
